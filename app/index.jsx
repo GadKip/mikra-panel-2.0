70ec03a40febca1c1b5da2902c57c61916f8c2c9
GadKip <gadavihai@gmail.com>
2024-11-29 00:05:21 +0200

    things are looking up - style-wise

--- app/index.jsx
+++ app/index.jsx
@@ -1,30 +1,30 @@
-import { Link } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { Pressable, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Pressable, ScrollView, Text, TextInput, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context'
 import CustomButton from '../components/CustomButton';
 
 export default function App() {
   return (
-    <SafeAreaView className="h-full bg-cyan-950">
-      <ScrollView contentContainerStyle={{height: '100%'}}>
-        <View className="w-full justify-center items-center h-full px-4">
-          <Text className="text-4xl text-gray-50">מקרא מבואר - ניהול</Text>
-          <TextInput
-            placeholder="שם משתמש"
-          />
-          <TextInput
-            placeholder="סיסמה"
-          />
-          <CustomButton />
-          <TouchableOpacity>
 
-          </TouchableOpacity>
-
-
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+<SafeAreaView className="flex-1 bg-cyan-950">
+  <ScrollView contentContainerStyle={{ height: '100%' }}>
+    <View className="flex-1 justify-center items-center px-4">
+      <Text dir="rtl" className="text-4xl text-gray-50">מקרא מבואר - ניהול</Text>
+      <View className="w-full max-w-xs mb-4">
+        <TextInput dir="rtl" className="border-b-2 border-r-4 text-2xl text-gray-50 w-full" placeholder="שם משתמש" />
+      </View>
+      <View className="w-full max-w-xs mb-4">
+      <TextInput dir="rtl" className="mt-1 border-b-2 border-r-4 text-2xl text-gray-50" placeholder="סיסמה" />
+      </View>
+      
+      
+      <CustomButton 
+      title="התחבר"
+      handlePress={() =>{}}
+      containerStyles="w-full mt-7"
+      /> 
+    </View>
+  </ScrollView>
+</SafeAreaView>
   );
 }
 
--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -1,11 +1,20 @@
-import { View, Text } from 'react-native'
+import { Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 
-const CustomButton = () => {
+const CustomButton = ({title, handlePress, containerStyles, textStyles, isLoading}) => {
   return (
-    <View>
-      <Text>CustomButton</Text>
-    </View>
+    <TouchableOpacity 
+    onPress={handlePress}
+    activeOpacity={0.7}
+    className={`bg-stone-50 rounded-xl min-h-12 min-w-40 justify-center items-center
+    ${containerStyles} ${isLoading ? 'opacity-50' : ''}`}
+    disabled={isLoading}>
+      <Text 
+      className={`text-primary text-2xl ${textStyles}`}
+      dir="rtl">
+      התחברות
+      </Text>
+    </TouchableOpacity>
   )
 }
 
