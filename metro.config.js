47a60b645fdca0c30730e13e3100bca56a83e433
GadKip <gadavihai@gmail.com>
2025-04-01 20:57:25 +0300

    build fix 6

--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,9 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-// Use a simpler configuration for production builds
-if (process.env.NODE_ENV === 'production') {
-  module.exports = config;
-} else {
-  // Use NativeWind only in development
-  const { withNativeWind } = require("nativewind/metro");
-  module.exports = withNativeWind(config, { input: "./global.css" });
-}
+// Always use NativeWind
+module.exports = withNativeWind(config, { input: './global.css' });
